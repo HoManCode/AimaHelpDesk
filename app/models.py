b85390067100a9cd015b5ab6ecf6e2d8ae15500c
HoomanDevOps <59225708+HoomanDevOps@users.noreply.github.com>
2020-09-11 23:43:13 +1000

    additional ticket's fileds are added

--- app/models.py
+++ app/models.py
@@ -6,7 +6,10 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     category=db.Column(db.String(100))
+    department=db.Column(db.String(100))
+    employee=db.Column(db.String(100))
+    tel=db.Column(db.Integer)
     description = db.Column(db.String(512))
 
     def __repr__(self):
-        return '<Task %r - %r>' % self.name, self.category, self.description
+        return '<Task %r - %r - %r - %r - %r - %r>' % self.name, self.category, self.department, self.employee, self.tel, self.description
--- app/routes.py
+++ app/routes.py
@@ -16,11 +16,14 @@ def add_item():
     # Get data from form fields taskName and taskDescription
     taskName = request.form.get('taskName')
     taskCategory=request.form.get('taskCategory')
+    taskDepartment=request.form.get('taskDepartment')
+    taskEmployee=request.form.get('taskEmployee')
+    taskTel=request.form.get('taskTel')
     taskDescription = request.form.get('taskDescription')
     
     # Put data into a new Task item
-    new_item = Task(name=taskName, category=taskCategory, description=taskDescription)
-    
+    new_item = Task(name=taskName, category=taskCategory, department=taskDepartment, employee=taskEmployee, tel=taskTel, description=taskDescription)
+
     # Add and commit the changes to the database
     db.session.add(new_item)
     db.session.commit()
@@ -51,17 +54,23 @@ def delete_task(id):
 def view_task(id):
     if (request.method == "GET"):
         task = Task.query.filter_by(id=id).first()
-        return render_template('view_task.html', taskName=task.name, taskCategory=task.category, taskDescription=task.description, taskId=task.id)
+        return render_template('view_task.html', taskName=task.name, taskCategory=task.category, taskDepartment=task.department, taskEmployee=task.employee, taskTel=task.tel , taskDescription=task.description, taskId=task.id)
     elif (request.method == "POST"):
         taskId = request.form.get('taskId')
         taskName = request.form.get('taskName')
         taskCategory=request.form.get('taskCategory')
+        taskDepartment=request.form.get('taskDepartment')
+        taskEmployee=request.form.get('taskEmployee')
+        taskTel=request.form.get('taskTel')
         taskDescription = request.form.get('taskDescription')
 
         task = Task.query.filter_by(id=id).first()
         if (task != None):
             task.name = taskName
             task.category=taskCategory
+            task.department=taskDepartment
+            task.employee=taskEmployee
+            task.tel=taskTel
             task.description = taskDescription
             db.session.add(task)
             db.session.commit()
